2c413701430f8db9becde3a1e776e6f89d288464
orangeji11 <jizongyao@uniontech.com>
2023-04-07 16:58:15 +0800

    sw64 support 2

--- cpuinfo_others.go
+++ cpuinfo_others.go
@@ -12,7 +12,7 @@
 // limitations under the License.
 
 // +build linux
-// +build !386,!amd64,!arm,!arm64,!mips,!mips64,!mips64le,!mipsle,!ppc64,!ppc64le,!riscv64,!s390x,!loong64
+// +build !386,!amd64,!arm,!arm64,!mips,!mips64,!mips64le,!mipsle,!ppc64,!ppc64le,!riscv64,!s390x,!loong64,!sw64
 
 package procfs
 
